274e48b4e53cf497b093a9c84164677579a18315
Viktor Smari <viktorsmari@gmail.com>
2016-12-14 19:57:08 +0100

    Add filtering for bikes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-
+var myapp = new Vue({
+  el: '#app',
+  data:{
+    showM: true,
+    showR: true,
+    showC: true,
+    bicycles:[
+    { gears:5, mountain:true,  road:false, cyclo:false, url:"img/bike1.png" },
+    { gears:3, mountain:true,  road:false, cyclo:false, url:"img/bike2.png" },
+    { gears:1, mountain:false, road:false, cyclo:true, url:"img/bike3.png" },
+    { gears:1, mountain:false, road:true,  cyclo:false, url:"img/bike4.png" },
+    ],
+  },
+})
